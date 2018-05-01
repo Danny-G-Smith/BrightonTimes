0ba369522907c59ff546a74690986b6afb7ebda6
Danny G Smith <dannygsmith@gmail.com>
2018-05-01 13:17:29 -0500

    added breakpoints and mobile flyout menu

---
+++ js/main.js
@@ -0,0 +1,25 @@
+;(function ( $, window, document, undefined ) {
+   'use strict';
+
+   jQuery(document).ready(function( $ ) {
+
+      var $menu   = $('#menu');
+      var $main   = $('main');
+      var $drawer = document.querySelector('.nav');
+
+      $menu.on('click', function(e) {
+         $drawer.classList.toggle('open');
+         e.stopPropagation();
+      });
+
+      $('#rotate').click(function(){
+         $(this).toggleClass('rotated');
+      });
+
+      $main.on('click', function() {
+         $drawer.removeClass('open');
+      });
+   });
+
+})
+(jQuery, window, document);
